c3854538372893b35f98603ece1add43a352ba6a
brettgrigsby <gbrett@gmail.com>
2015-06-11 15:18:39 -0600

    add user model

---
+++ spec/features/register_new_user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'user registration view', type: :feature do
+
+  before(:each) do
+    visit new_user_path
+  end
+
+  it 'creates a new user' do
+    page.click_link('New User')
+    page.fill_in('Name', with: 'Bob')
+    page.fill_in('Password', with: 'pass')
+    page.fill_in('Password Confirmation', with: 'pass')
+    page.click_button('Create User')
+    expect(page).to have_content("Bob's Ideas")
+  end
+end
