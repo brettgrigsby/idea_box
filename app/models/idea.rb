f909baedef5cb00cd05713309641bba0fbc291af
brettgrigsby <gbrett@gmail.com>
2015-06-14 17:38:20 -0600

    push before we split up

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,7 @@ class Idea < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
   belongs_to :user
+  belongs_to :category
   has_many :idea_images
   has_many :images, through: :idea_images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614214933) do
+ActiveRecord::Schema.define(version: 20150614232323) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -32,11 +32,13 @@ ActiveRecord::Schema.define(version: 20150614214933) do
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
+  add_index "ideas", ["category_id"], name: "index_ideas_on_category_id"
   add_index "ideas", ["user_id"], name: "index_ideas_on_user_id"
 
   create_table "images", force: :cascade do |t|
