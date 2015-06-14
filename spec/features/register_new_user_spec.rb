a6e1b6930bca2fd8c379e0fdac2d7b8b371ccf06
brettgrigsby <gbrett@gmail.com>
2015-06-14 11:10:10 -0600

    add sad path spec for user creation

--- spec/features/register_new_user_spec.rb
+++ spec/features/register_new_user_spec.rb
@@ -14,4 +14,9 @@ describe 'user registration view', type: :feature do
     expect(page).to have_content("Bob's Ideas")
   end
 
+  it 'will not create a user without a name' do
+    page.fill_in('Password', with: 'pass')
+    page.click_button('Create User')
+    expect(page).to have_content("Name can't be blank")
+  end
 end
