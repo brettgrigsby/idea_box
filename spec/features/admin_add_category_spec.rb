ae1c918585ff5e21318c90c393bee665a2e9f5e5
brettgrigsby <gbrett@gmail.com>
2015-06-14 14:56:39 -0600

    add spec for category create and delete

--- spec/features/admin_add_category_spec.rb
+++ spec/features/admin_add_category_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
-describe 'admin Login', type: :feature do
+describe 'categories', type: :feature do
 
   before(:each) do
     User.create(name: 'admin', role: 1, password: 'pass')
     visit root_path
-  end
-
-  it 'takes a registered admin to the admin user page' do
     page.fill_in('Name', with: 'admin')
     page.fill_in('Password', with: 'pass')
     page.click_button('Login')
-    expect(page).to have_content("Categories")
   end
 
+  it 'creates new categories' do
+    page.click_link 'Categories'
+    page.click_link 'Create Category'
+    page.fill_in('Title', with: 'Whatever')
+    page.click_button('Create Category')
+    expect(page).to have_content('Whatever')
+  end
+
+  it 'deletes categories' do
+    page.click_link 'Categories'
+    page.click_link 'Create Category'
+    page.fill_in('Title', with: 'Whatever')
+    page.click_button('Create Category')
+    page.click_link('Delete')
+    expect(page).to have_content("Whatever was destroyed!!")
+  end
 end
