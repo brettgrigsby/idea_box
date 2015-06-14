9037deffb02aedc93b255fb362c5188ab5bc1ae6
brettgrigsby <gbrett@gmail.com>
2015-06-14 13:19:39 -0600

    make default role 0

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611204628) do
+ActiveRecord::Schema.define(version: 20150614191719) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.integer  "role"
+    t.integer  "role",            default: 0
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
